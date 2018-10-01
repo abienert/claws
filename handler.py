7e58b0f2c51b9c6cbfa9c5bf413b7fe1d6e6aaa8
Andrew Bienert <abienert@seek.com.au>
2018-10-01 15:45:49 +1000

    Debugging lambda.

--- handler.py
+++ handler.py
@@ -3,7 +3,8 @@ import subprocess
 
 def cli(event, context):
 
-    print(subprocess.check_output(['./aws', 'l']))
+    print(subprocess.check_output(['ls', '-al']))
+    #print(subprocess.check_output(['./aws', 'l']))
 
     return {
         "message": "Go Serverless v1.0! Your function executed successfully!",
